53b3b0209fadf1865d016e4a73f3a2e8dc77bd6a
guillermo.moreno <guillermo.moreno@digital.hmrc.gov.uk>
2017-03-23 15:19:41 +0000

    first commit: bootstrap, gulp, express

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+var config = {
+  bootstrapDir: './node_modules/bootstrap-sass',
+  publicDir: './public'
+}
+
+gulp.task('css', function() {
+    return gulp.src(config.publicDir + '/dev/stylesheets/style.scss')
+    .pipe(sass({
+        includePaths: [
+          config.bootstrapDir + '/assets/stylesheets',
+          // config.publicDir + '/dev/fonts/scss'
+        ],
+    }))
+    .pipe(gulp.dest(config.publicDir + '/stylesheets'));
+});
+
+gulp.task('fonts', function() {
+    return gulp.src(config.bootstrapDir + '/assets/fonts/**/*')
+    .pipe(gulp.dest(config.publicDir + '/fonts'));
+});
+
+gulp.task('default', ['css', 'fonts']);
